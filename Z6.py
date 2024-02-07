21854bfa5226c7cb0358cd9af49935a539d635a7
Anutik55860 <158280420+Anutik55860@users.noreply.github.com>
2024-02-07 13:49:39 +0300

    Create Z6.py

---
+++ Z6.py
@@ -0,0 +1,19 @@
+import math
+from maht import cos, sin
+#1)
+k=int ( input("k:"))
+A= pow(100,3)-pow(92,2)+k 
+print (A)
+#2)
+X=int(input "x:")
+В=sqrt((1-cos(x))/2)
+print (B)
+#3)
+X=int(input"x2")
+C= sqrt(fabs(x2)*log(x2)pow(x2,2)/-5/4x+e pow(e,x/2)
+print (C) 
+#4)
+X= int(input"x:")
+Y= int(input"y:")
+D=(sqrt(pow(sin (x,2))+(cos (y,3),2))
+print (D)
